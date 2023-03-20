dbd44360441ef003b97ed35b8cae7cbe81149b7d
Galen <gmitt98@users.noreply.github.com>
2023-03-19 22:59:29 -0700

    basic dataframe stats exploration

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,10 +11,33 @@ file_path = os.path.join(project_dir, "Resources", "budget_data.csv")
 # Now I read the CSV file into a DataFrame
 df = pd.read_csv(file_path)
 
+#print(df.__len__())
+
 # These date formats did not play nice with regex to read the month value, so I went and found another solution using a new library
 # Here I am using the parser module to add a column to my dataframe that is just the month
 # I'm applying a lambda function using this module to each value in the date column and writing result that to the month column
 df['Month'] = df['Date']. apply(lambda x: parse(x).month)
-#print(df)
+df['Year'] = df['Date'].apply(lambda x: parse(x).year)
+print(df)
+# Now I can read the number of unique months in the file
 monthCount = df['Month'].nunique()
-print(monthCount)
+print(monthCount) #prints '12', yay
+yearCount = df['Year'].nunique()
+monthCountTotal = df['Date'].nunique()
+print(monthCountTotal)
+print(yearCount)
+# Clean up these comments, but... inspecting this data we see that there are only 2023 dates here
+# So there are only 12 months!
+
+totalSum = df['Profit/Losses'].sum()
+print(totalSum)
+
+profitDiff = df['Profit/Losses'].diff()
+print(profitDiff)
+avgChange = profitDiff.mean()
+print(avgChange)
+maxIncrease = profitDiff.max()
+minIncrease = profitDiff.min()
+
+print(maxIncrease)
+print(minIncrease)
